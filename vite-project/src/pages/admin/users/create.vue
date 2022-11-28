f57b089919b21611ab2e7578f9e15f078a6493c4
Hoang Cong Dung <hcdung2109@gmail.com>
2022-11-28 16:55:21 +0700

    update

---
+++ vite-project/src/pages/admin/users/create.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>page create user</h1>
+</template>>
--- vite-project/src/router/admin.js
+++ vite-project/src/router/admin.js
@@ -1,13 +1,30 @@
 const admin = [
   {
     path: "/admin",
-    name: "name",
+    name: "admin",
     component: () => import("../layouts/admin.vue"),
     children: [
       {
         path: "users",
         name: "admin-users",
         component: () => import("../pages/admin/users/index.vue"),
+        children: [
+          {
+          path: "create",
+          name: "admin-users-create",
+          component: () => import("../pages/admin/users/create.vue"), 
+          },
+          {
+            path: "edit",
+            name: "admin-users-edit",
+            component: () => import("../pages/admin/users/edit.vue"), 
+            },
+      ]
+      },
+      {
+        path: "contacts",
+        name: "admin-contacts",
+        component: () => import("../pages/admin/contacts/index.vue"),
       },
     ],
   },
